715be241ac85c390caf064b65e491918d8285807
Lisa DeSpain <lisamdespain@gmail.com>
2022-05-13 17:45:03 -0500

    finally something is working at 39 passed

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,4 +1,47 @@
 // Write your tests here
+import React from 'react';
+import { render, fireEvent, screen } from '@testing-library/react'
+import AppClass from './AppClass';
+
+
+
 test('sanity', () => {
-  expect(true).toBe(false)
+  expect(true).toBe(true);
+})
+
+test('AppClass loads', () =>{
+render(<AppClass />);
+})
+
+test('We have a header', () =>{
+  render(<AppClass />);
+  const header = screen.getByText(/Welcome to the Grid/i)
+  expect(header).toBeInTheDocument();
+})
+
+test('Button text exists', () =>{
+    render(<AppClass />);
+    const rightButton = screen.getByText(/right/i)
+    expect(rightButton).toBeInTheDocument();
+    const leftButton = screen.getByText(/left/i)
+    expect(leftButton).toBeInTheDocument();
+    const downButton = screen.getByText(/down/i)
+    expect(downButton).toBeInTheDocument();
+    const upButton = screen.getByText(/up/i)
+    expect(upButton).toBeInTheDocument();
+    const resetButton = screen.getByText(/reset/i)
+    expect(resetButton).toBeInTheDocument();
+})
+
+test('Link text exists', () =>{
+      render(<AppClass />);
+      const functionalLink = screen.getByDisplayValue(/functional/i)
+      expect(functionalLink).toBeInTheDocument();
+})
+
+test('can type in the email field', () =>{
+  render(<AppClass />);
+  const emailInput = screen.getByPlaceholderText(/type email/i)
+  fireEvent.type(emailInput, { target: { value: 'lisamdespain@gmail.com' } })
+  expect(emailInput).toHaveValue('lisamdespain@gmail.com')
 })
--- frontend/components/AppClass.js
+++ frontend/components/AppClass.js
@@ -1,34 +1,130 @@
 import React from 'react'
 
 export default class AppClass extends React.Component {
+  state = {
+    x: 2,
+    y: 2,
+    message: '',
+    grid: ['','','','','B','','','',''],
+    totalTurns: 0,
+    activeSquare: false
+  }
+  leftClick = (x, y) =>{
+    if (x === 2 || x === 3) {
+      this.setState({
+        x: x - 1});
+        return this.newIdx(x, y)
+      } else {
+        return this.setState({
+          message: "You can't go left"})
+      }
+  }
+
+  rightClick = (x, y) =>{
+    if (x === 1 || x === 2) {
+      this.setState({
+        x: x + 1});
+        return this.newIdx(x, y)
+      } else {
+        return this.setState({
+          message: "You can't go right"
+        })
+      }
+  }
+
+  upClick = (x, y) =>{
+    if (y === 2 || y === 3) {
+      this.setState({
+        y: y - 1});
+        return this.newIdx(x, y)
+      } else {
+        this.setState({
+          message: "You can't go up"
+        })      }
+  }
+
+  downClick = (x, y) =>{
+    if (y === 1 || y === 2) {
+      this.setState({
+        y: y + 1});
+      return this.newIdx(x, y)
+      } else {
+        this.setState({
+          message: "You can't go down"
+        })
+      }
+  }
+
+  resetClick = () =>{
+    return this.state.grid;
+  }
+makeSquareActive = (id) =>{
+  id ? this.setState({
+    activeSquare: true
+  }) : false;
+}
+newIdx = (x, y) =>{
+  if (x === 1 && y === 1){
+    this.setState({
+      grid: ['B','','','','','','','','']});
+    return this.makeSquareActive(0);
+} else if (x === 2 && y === 1){
+  this.setState({
+    grid: ['','B','','','','','','','']});
+  return this.makeSquareActive(1);
+} else if (x === 3 && y === 1){
+  this.setState({
+    grid: ['','','B','','','','','','']});
+  return this.makeSquareActive(2);
+} else if (x === 1 && y === 2){
+  this.setState({
+    grid: ['','','','B','','','','','']});
+  return this.makeSquareActive(3);
+} else if (x === 2 && y === 2){
+  this.setState({
+    grid: ['','','','','B','','','','']});
+  return this.makeSquareActive(4);
+} else if (x === 3 && y === 2){
+  this.setState({
+    grid: ['','','','','','B','','','']});
+  return this.makeSquareActive(5);
+} else if (x === 1 && y === 3){
+  this.setState({
+    grid: ['','','','','','','B','','']});
+  return this.makeSquareActive(6);
+} else if (x === 2 && y === 3){
+  this.setState({
+    grid: ['','','','','','','','B','']});
+  return this.makeSquareActive(7);
+}else if (x === 3 && y === 3){
+  this.setState({
+    grid: ['','','','','','','','','B']});
+  return this.makeSquareActive(8);
+}
+}
+
   render() {
+    
     const { className } = this.props
     return (
       <div id="wrapper" className={className}>
         <div className="info">
-          <h3 id="coordinates">Coordinates (2, 2)</h3>
-          <h3 id="steps">You moved 0 times</h3>
+          <h3 id="coordinates">{`Coordinates (${this.state.x}, ${this.state.y})`}</h3>
+          <h3 id="steps">{`You moved ${this.state.totalTurns} times`}</h3>
         </div>
-        <div id="grid">
-          <div className="square"></div>
-          <div className="square"></div>
-          <div className="square"></div>
-          <div className="square"></div>
-          <div className="square active">B</div>
-          <div className="square"></div>
-          <div className="square"></div>
-          <div className="square"></div>
-          <div className="square"></div>
+          <div id="grid">
+          {this.state.grid.map((square, idx) => <div key={idx} className="square">{square}</div>)}
+ 
         </div>
         <div className="info">
-          <h3 id="message"></h3>
+          <h3 id="message">{this.state.message}</h3>
         </div>
         <div id="keypad">
-          <button id="left">LEFT</button>
-          <button id="up">UP</button>
-          <button id="right">RIGHT</button>
-          <button id="down">DOWN</button>
-          <button id="reset">reset</button>
+          <button onClick={() => this.leftClick(this.state.x, this.state.y)} id="left">LEFT</button>
+          <button onClick={() => this.upClick(this.state.x, this.state.y)} id="up">UP</button>
+          <button onClick={() => this.rightClick(this.state.x, this.state.y)} id="right">RIGHT</button>
+          <button onClick={() => this.downClick(this.state.x, this.state.y)} id="down">DOWN</button>
+          <button onClick={() => this.resetClick()} id="reset">reset</button>
         </div>
         <form>
           <input id="email" type="email" placeholder="type email"></input>
